cd492e84c471a51155705c963819fda245690e69
ViaYuSecE <n01679708@humber.ca>
2024-10-08 09:15:39 -0400

    Create lab6.js

---
+++ lab6.js
@@ -0,0 +1,29 @@
+//Task 1
+let myNumber = 10;
+let myString = '"Hello, Javascript!"';
+console.log(`myNumber is ${myNumber} and myString is ${myString}`);
+
+
+//Task 2_1
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+
+//Task 2_2
+for (let i = 1; i <= 5; i++) {
+    console.log(i * 3);
+}
+
+
+//Task 3
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log(addNumbers(50, 100));
+
+
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiplyNumbers(50, 100));
